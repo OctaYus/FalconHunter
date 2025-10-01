2d000411478778b50ab564208eaf6001a6f9c115
octayus <1myousefxdf281@gmail.com>
2025-10-01 04:26:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1214,7 +1214,7 @@ class TelegramNotify:
         url = f"https://api.telegram.org/bot{token}/sendMessage"
         payload = {"chat_id": chat_id, "text": message, "parse_mode": "HTML"}
 
-        response = requests.post(url, json=payload)
+        response = requests.post(url, json=payload, timeout=3)
 
         if response.status_code == 200:
             logger.info("Notification sent successfully!")
